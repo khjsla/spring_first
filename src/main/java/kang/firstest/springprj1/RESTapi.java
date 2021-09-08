ee33ccb3c92d07ea45814ffb5033ad31619a370d
khjsla <rkdguswlsla@naver.com>
2021-09-08 17:03:36 +0900

    makin get api to get pharmacy api data

--- src/main/java/kang/firstest/springprj1/RESTapi.java
+++ src/main/java/kang/firstest/springprj1/RESTapi.java
@@ -4,28 +4,53 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.type.TypeReference;
 
+import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Map;
 
 @RestController
 public class RESTapi {
 
     @GetMapping("/getTest")
-    public String welcome(){
+    public String welcome() {
         return "server is running well XD";
     }
 
     @PostMapping("/postTest")
-    public Object hello(@RequestBody String pBody) throws JsonProcessingException{
+    public Object hello(@RequestBody String pBody) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> map = mapper.readValue(pBody, new TypeReference<Map<String, Object>>() {});
-        Map<String, Object> userRequest = ((Map<String,Object>) map.get("userRequest"));
+        Map<String, Object> map = mapper.readValue(pBody, new TypeReference<Map<String, Object>>() {
+        });
+        Map<String, Object> userRequest = ((Map<String, Object>) map.get("userRequest"));
         return userRequest.get("utterance");
     }
     // pBody - input JSON
     // pBody 의 userRequest 의 utterance 를 get
+
+    @GetMapping("/getApiTest")
+    public String publicApi() {
+        String uri = "https://api.odcloud.kr/api/15066537/v1/uddi:3fcefe25-765d-41f2-8540-8c6df41d54bd?page=1&perPage=128";
+        uri += "&format=json";
+        // i can get pharmacy data from above
+
+        try {
+            URL url = new URL(uri);
+            URLConnection connection = url.openConnection();
+
+
+        } catch (IOException e) {
+
+        }
+
+        return "server is running well XD";
+    }
 }
